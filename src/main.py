f0844191c2c8a59b382524799aee03d5c6d8f756
Shivang Jha <shivang.jha@hestabit.in>
2026-01-27 17:05:15 +0530

    Day 3 Tasks: Build Three tool calling agents

--- src/main.py
+++ src/main.py
@@ -20,6 +20,8 @@ asyncio.run(main())
 
 '''
 
+'''
+Day 2
 
 import asyncio
 from autogen_ext.models.llama_cpp import LlamaCppChatCompletionClient
@@ -57,6 +59,24 @@ async def main():
         print(f"Node: {node_id}")
         print(f"  Deps  : {data['deps']}")
         print(f"  Output:\n{data['output']}\n")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+'''
+
+from orchestrator import run_orchestration,summarize_results
+from agents.answer_agent import answer_agent
+import asyncio
+
+async def main():
+    user_query = "Analyze sales.csv and generate top 5 insights"
+    context = await run_orchestration(user_query)
+    final_summary = summarize_results(context)
+    task = f"You have to reply to user query: {user_query}, based on the context available below: \n{final_summary}"
+    result = await answer_agent.run(task=task)
+    print("=== Final Agent Outputs ===")
+    print(result.messages[-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
